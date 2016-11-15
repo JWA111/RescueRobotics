f43a069cf8dc4f179c60b9b40b8abf4dd134bf89
Zach Brotman <Zach Brotman>
2016-11-14 19:41:54 -0700

    Client Side JS code

--- kinect_data_stream.js
+++ kinect_data_stream.js
@@ -1,9 +1,10 @@
 // following code is from:
 // http://www.webondevices.com/get-started-with-xbox-kinect-2-javascript-development/
 
+// modify so that the method runs when it 
+// recieves a request from the web socket
 var Kinect2 = require('kinect2');
 var kinect = new Kinect2();
-var fs = require('fs');
 
 {
     if(kinect.open()) 
@@ -12,20 +13,25 @@ var fs = require('fs');
         //listen for body frames
         kinect.on('depthFrame', function(depthFrame)
         {
-            fs.writeFile('out.jpg', depthFrame, function(error) {
-                console.error(error);
-            });
+            bufferString = JSON.stringify(depthFrame.toJSON());
+            console.log(JSON.parse(bufferString));
             kinect.close();
             process.exit();
         });
 
         //request body frames
         kinect.openDepthReader();
-
+        console.log('Request Depth Frames')
         //close the kinect after 5 seconds
         setTimeout(function(){
+            kinect.closeDepthReader();
             kinect.close();
             console.log("Kinect Closed");
-        }, 60000);
+        }, 12000);
+
     }
+}
+
+{
+    
 }
