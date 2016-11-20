b6aba5f2f48c308c9c4b26c91d015d265e55adc9
JWA111 <jameswilliamadams11@gmail.com>
2016-11-19 18:30:35 -0700

    Merge Kinect code with servers code

--- CommandServer.js
+++ CommandServer.js
@@ -4,6 +4,7 @@ var os = require('os');
 
 var ifaces = os.networkInterfaces();
 var NAME = 'commander';
+var objective = 256;
 
 // Get ip
 var ip;
@@ -33,9 +34,8 @@ var server = ws.createServer(function (conn) {
             }
             if (message.depth) {
                 var sent = false;
-                // TODO calc dt and send to robot;
-                var dt = 0;
                 console.log('depth', message.depth.length, message.depth[0].length);
+                var dt = calcDT(message.depth, objective);
                 server.connections.forEach(function(connection) {
                     if (connection.dev === 'robot') {
                         connection.sendText(JSON.stringify({'dev': NAME, 'dt': dt}));
@@ -67,3 +67,43 @@ var server = ws.createServer(function (conn) {
     });
 }).listen(port)
 console.log('listening on ' + ip + ':' + port);
+
+function calcDT(depthArry, objective){
+  depthSum = [];
+  depthFrame = depthArry;
+  obj = objective;
+  const MAX = depthFrame.length;
+  const MIDDLE = MAX/2;
+
+  var trajectory = obj;
+  var leftIterator = obj;
+  var rightIterator = obj;
+  
+  if(obj<=0 || obj>=MAX){
+      return obj - MIDDLE;
+  }
+
+  for(row of depthFrame) {
+      for(var i = 0; i < row.length; i++){
+          if(row[i] == 0){
+              depthSum[i] = 0;
+          }
+          else depthSum[i] = MAX - row
+      }
+  }
+
+  while(leftIterator >=0 && rightIterator <= depthFrame[0].length){
+      if(depthSum[leftIterator]<=depthSum[trajectory]){
+          trajectory = leftIterator;
+      }
+      if(depthSum[rightIterator]<=depthSum[trajectory]){
+          trajectory = rightIterator;
+      }
+      if(depthSum[trajectory] == 0){
+          break;
+      }
+      leftIterator--;
+      rightIterator++;
+  }
+  return trajectory-MIDDLE;
+}
--- KinectServer.js
+++ KinectServer.js
@@ -1,24 +1,53 @@
 var ws = require('nodejs-websocket');
+var Rx = require('@reactivex/rxjs');
+var Kinect2 = require('kinect2');
+
+var kinect = new Kinect2();
 
 var commandHost = 'ws://10.140.171.242:8001';
-var connection = ws.connect(commandHost);
 var NAME = 'kinect';
+var depthArraySubject = new Rx.BehaviorSubject([[]]);
 
-connection.on('error', function (err) {
-    console.error(err);
-});
+if (kinect.open()) {
+    console.log("Kinect Opened");
+    //listen for body frames
+    kinect.on('depthFrame', function(depthFrame) {
 
-connection.on('text', function (str) {
-    const message = JSON.parse(str);
-    if (!message || !message.command) {
-        console.error("Recieved invalid message");
-    } else if (message.command == 'trajectory') {
-        const df = [[1,1,1,1]]; // TODO get df from kinect
-        connection.sendText(JSON.stringify({'dev': NAME, 'depth': df}));
-    }
-});
+        bufferJson = depthFrame.toJSON();
+        bufferString = JSON.stringify(bufferJson);
+        
+        bufferArray = JSON.parse(bufferString);
+        depthArray = [];
+        while (bufferArray.data.length) {
+            depthArray.push(bufferArray.data.splice(0,512));
+        }
+        depthArraySubject.next(depthArray);
+    });
 
-connection.on('connect', function () {
-    console.log('connected to command server');
-    connection.sendText(JSON.stringify({'dev': NAME}));
-});
+    //request body frames
+    kinect.openDepthReader();
+
+    setTimeout(function() {
+        var connection = ws.connect(commandHost);
+        connection.on('error', function (err) {
+            console.error(err);
+        });
+
+        connection.on('text', function (str) {
+            const message = JSON.parse(str);
+            if (!message || !message.command) {
+                console.error("Recieved invalid message");
+            } else if (message.command == 'trajectory') {
+                const df = depthArraySubject.value();
+                connection.sendText(JSON.stringify({'dev': NAME, 'depth': df}));
+            }
+        });
+
+        connection.on('connect', function () {
+            console.log('connected to command server');
+            connection.sendText(JSON.stringify({'dev': NAME}));
+        });
+    }, 5000);
+} else {
+    console.error("Failed to open kinect");
+}
